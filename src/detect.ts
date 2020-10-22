2a5e6ce502e815f03fa8a9b5e92bb480065b591c
ty <towyuan@outlook.com>
2020-10-22 11:43:44 +0800

    chore: reformat

--- src/detect.ts
+++ src/detect.ts
@@ -12,22 +12,26 @@ export interface Detected {
 export function detect(file: string, lang?: string): Detected | undefined {
   const extension = path.extname(file);
   const codec = findCodec({lang, extension});
-  if (!codec && lang) { // unsupported lang
+  if (!codec && lang) {
+    // unsupported lang
     return;
   }
 
-  if (codec && !lang) { // exactly match
+  if (codec && !lang) {
+    // exactly match
     return {file, lang: codec.lang, codec};
   }
 
-  if (codec) { // detect by lang
+  if (codec) {
+    // detect by lang
     for (const ext of codec.extensions) {
       const f = file + ext;
       if (fs.existsSync(f)) {
         return {file: f, lang: codec.lang, codec};
       }
     }
-  } else { // detect by supported codecs
+  } else {
+    // detect by supported codecs
     for (const c of Object.values(codecs)) {
       for (const ext of c.extensions) {
         const f = file + ext;
